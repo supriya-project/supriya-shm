ffa27bb157206bcd6ec1c939867a0e595576be97
Joséphine Wolf Oberholtzer <josephine.wolf.oberholtzer@gmail.com>
2026-03-15 14:29:04 -0400

    Package release process

--- src/supriya_shm/__init__.py
+++ src/supriya_shm/__init__.py
@@ -1,4 +1,9 @@
 from ._shm import ServerSHM
+from ._version import __version__, __version_info__
 
 
-__all__ = ["ServerSHM"]
+__all__ = [
+    "ServerSHM",
+    "__version__",
+    "__version_info__",
+]
---
+++ src/supriya_shm/_version.py
@@ -0,0 +1,12 @@
+"""
+Version information.
+
+- Major version is last two digits of the current year.
+- Minor version is the month as a digit.
+- Patch / beta tag is the number of releases during that month.
+
+This follows black's versioning scheme.
+"""
+
+__version_info__ = (26, "3b0")
+__version__ = ".".join(str(x) for x in __version_info__)
