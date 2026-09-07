7157813cbe0c8fe09afaa0add093af79959dfe0b
Joséphine Oberholtzer <josephine.wolf.oberholtzer@gmail.com>
2026-09-07 17:29:36 -0400

    Fixup dev/bump-version.py

--- dev/bump-version.py
+++ dev/bump-version.py
@@ -28,7 +28,7 @@ def rewrite_version_file(year: int, month: int, beta: int) -> None:
 
 def update_pyproject_toml(year: int, month: int, beta: int) -> None:
     version = f"{year}.{month}b{beta}"
-    subprocess.run(["uv", "version", "--no-sync", version])
+    subprocess.run(["uv", "version", "--no-sync", version], check=True)
 
 
 def build_parser():
